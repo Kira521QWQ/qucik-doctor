f0f8a1a78c34b9d101e4a7c999f65e0fa4a431b7
s4s <the@163.com>
2022-12-23 17:38:38 +0800

    tabbar路由的使用配置-tabicon的定制

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,11 +2,47 @@
 
 <template>
   <div class="layout">
-    <div>不变的底部tabBar</div>
     <!-- Layout 中变化的路由  -->
+
     <RouterView></RouterView>
-    <div>不变</div>
+    <van-tabbar route>
+      <van-tabbar-item to="/home">
+        首页
+        <template #icon="{ active }">
+          <CpIcon :name="`home-index-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/article">
+        健康百科
+        <template #icon="{ active }">
+          <CpIcon :name="`home-article-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/notify">
+        消息中心
+        <template #icon="{ active }">
+          <CpIcon :name="`home-notice-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/user">
+        我的
+        <template #icon="{ active }">
+          <CpIcon :name="`home-mine-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout {
+  :deep() {
+    .van-tabbar-item__icon {
+      font-size: 21px;
+    }
+    .van-tabbar-item__text {
+      font-size: 11px;
+    }
+  }
+}
+</style>

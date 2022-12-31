c66804b79d4293011727f1561ef457dda4e80dae
s4s <the@163.com>
2022-12-31 10:49:14 +0800

    websocket api 基本使用体验

--- src/views/Test/TestPage.vue
+++ src/views/Test/TestPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onUnmounted } from 'vue';
+import { ref, onUnmounted, onMounted } from 'vue';
 import { instance as request } from '@/utils/request';
 
 const counter = ref(10);
@@ -32,6 +32,32 @@ const getUserInfo = async () => {
   // 发生了异常，这里没有执行，这是我们想要的效果
   console.log('请求后，并且没有异常走这里');
 };
+
+let ws: WebSocket;
+const msg = ref('');
+const txt = ref('');
+// 建立 webscoket 连接
+onMounted(() => {
+  ws = new WebSocket('ws://121.40.165.18:8800');
+
+  ws.addEventListener('open', () => {
+    console.log('连接建立');
+    // 发送消息
+    ws.send('Hello WebSockets!');
+  });
+
+  // 接受消息事件
+  ws.onmessage = function (evt) {
+    console.log('接收到的消息: ' + evt.data);
+    msg.value = msg.value + evt.data;
+    // 关闭连接
+    // ws.close();
+  };
+  // 关闭连接事件
+  ws.onclose = function (evt) {
+    console.log('连接断开');
+  };
+});
 </script>
 
 <template>
@@ -39,6 +65,11 @@ const getUserInfo = async () => {
   <button @click="getUserInfo">发起一个token过期的请求</button>
   <h2>{{ counter }}</h2>
   <button @click="startCounter">开启倒计时</button>
+  <hr />
+  <!-- <div>{{ msg }}</div> -->
+  <div v-html="msg" />
+  <input v-model="txt" />
+  <button @click="ws.send(txt)">发送消息</button>
 </template>
 
 <style lang="scss" scoped></style>

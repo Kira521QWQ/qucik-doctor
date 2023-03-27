14b355161e99f2d054c34d2f046d31500f3d9e0b
kiraqwq <lzzbangbangda@163.com>
2023-03-27 16:32:09 +0800

    first

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref, onUnmounted, onMounted } from 'vue';
+import { ref, onUnmounted, onMounted } from "vue";
 // 导入路由管理和路由实例
-import { useRouter, useRoute } from 'vue-router';
+import { useRouter, useRoute } from "vue-router";
 // 导入保存用户信息的store
-import { useUserStore } from '@/stores/user';
+import { useUserStore } from "@/stores/user";
 // 业务网络请求
-import { loginByPassword, loginByCode, getSMSCode } from '@/services/user';
-import { Toast, type FormInstance } from 'vant';
+import { loginByPassword, loginByCode, getSMSCode } from "@/services/user";
+import { Toast, type FormInstance } from "vant";
 
 // 创建userStore
 const userStore = useUserStore();
@@ -16,22 +16,24 @@ const route = useRoute();
 
 // 父组件绑定子组件暴露的事件处理函数
 const onClickRight = () => {
-  console.log('右侧点击，父组件执行的事件处理函数');
+  console.log("右侧点击，父组件执行的事件处理函数");
   userStore.delUser();
 };
 
 // 搜集页面数据（定义数据容器）：手机号和密码
-const mobile = ref('');
-const password = ref('');
+const mobile = ref("");
+const password = ref("");
+
 // 页面状态数据
 const isAgree = ref(false);
 const isShowPwd = ref(false);
+
 // 登录方式的条件
 const isPwdLogin = ref(true);
 // vant form 实例变量
 const form = ref<FormInstance>();
 // 验证码
-const smsCode = ref('');
+const smsCode = ref("");
 // 倒计时页面响应式数据
 const counter = ref(0);
 // 倒计时的timerId
@@ -39,23 +41,23 @@ let timerId: number;
 
 // 配置表单校验，对象数组
 const mobileRules = [
-  { required: true, message: '请输入手机号' },
-  { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确' },
+  { required: true, message: "请输入手机号" },
+  { pattern: /^1[3-9]\d{9}$/, message: "手机号格式不正确" },
 ];
 // 密码校验规则
 const passwordRules = [
-  { required: true, message: '请输入密码' },
-  { pattern: /^\w{8,24}$/, message: '密码需8-24个字符' },
+  { required: true, message: "请输入密码" },
+  { pattern: /^\w{8,24}$/, message: "密码需8-24个字符" },
 ];
 // 验证码规则
 const codeRules = [
-  { required: true, message: '请输入验证码' },
-  { pattern: /^\d{6}$/, message: '验证码6位数字' },
+  { required: true, message: "请输入验证码" },
+  { pattern: /^\d{6}$/, message: "验证码6位数字" },
 ];
 
 // 登录事件处理
 const login = async (formValues: { mobile: string; password: string }) => {
-  if (!isAgree.value) return Toast('请勾选已同意');
+  if (!isAgree.value) return Toast("请勾选已同意");
 
   // 判断用户登录方式
   // 密码登录
@@ -74,9 +76,9 @@ const login = async (formValues: { mobile: string; password: string }) => {
       router.push(route.query.returnURL as string);
     } else {
       // 去个人页面
-      router.push('/user');
+      router.push("/user");
     }
-    Toast.success('登录成功');
+    Toast.success("登录成功");
   } else {
     // 验证码登录
     const user = await loginByCode(formValues.mobile, smsCode.value);
@@ -86,14 +88,14 @@ const login = async (formValues: { mobile: string; password: string }) => {
       router.push(route.query.returnURL as string);
     } else {
       // 去个人页面
-      router.push('/user');
+      router.push("/user");
     }
-    Toast.success('登录成功');
+    Toast.success("登录成功");
   }
 };
 // 发送验证码的事件处理函数
 const sendSms = () => {
-  console.log('对手机号合法性验证开始');
+  console.log("对手机号合法性验证开始");
   // 1、promise 异常的 async 的try catch 处理方式
   // try {
   //   // 手动执行表单指定的验证
@@ -115,9 +117,9 @@ const sendSms = () => {
   //     console.log('手机号有问题', error);
   //   });
   // 3、.then 的第二个参数就是异常处理
-  form.value?.validate('mobile').then(
+  form.value?.validate("mobile").then(
     async (resolve) => {
-      console.log('验证通过resolve', resolve);
+      console.log("验证通过resolve", resolve);
       // 如果当前计时中，则返回
       if (counter.value > 0) return;
 
@@ -136,11 +138,11 @@ const sendSms = () => {
       }, 1000);
 
       // 告诉服务器，发送验证码
-      const code = await getSMSCode(mobile.value, 'login');
+      const code = await getSMSCode(mobile.value, "login");
       console.log(code);
     },
     (reject) => {
-      console.log('手机号有问题reject', reject);
+      console.log("手机号有问题reject", reject);
     }
   );
 };
@@ -164,9 +166,9 @@ onMounted(() => {
     <CpNavBar title="登录" rightText="注册" @click-right="onClickRight" />
     <!-- 登录方式 -->
     <div class="login-head">
-      <span class="title">{{ isPwdLogin ? '密码登录' : '短信登录' }}</span>
+      <span class="title">{{ isPwdLogin ? "密码登录" : "短信登录" }}</span>
       <a class="sms" href="javascript:;" @click="isPwdLogin = !isPwdLogin">
-        <span>{{ isPwdLogin ? '短信验证码登录' : '密码登录' }}</span>
+        <span>{{ isPwdLogin ? "短信验证码登录" : "密码登录" }}</span>
         <van-icon name="arrow"></van-icon>
       </a>
     </div>
@@ -204,9 +206,11 @@ onMounted(() => {
           name="smsCode"
         >
           <template #button>
-            <span :class="['btn-send', counter > 0 ? 'disabled' : '']" @click="sendSms">{{
-              counter > 0 ? counter + 's之后发送' : '发送验证码'
-            }}</span>
+            <span
+              :class="['btn-send', counter > 0 ? 'disabled' : '']"
+              @click="sendSms"
+              >{{ counter > 0 ? counter + "s之后发送" : "发送验证码" }}</span
+            >
           </template>
         </van-field>
         <div class="cp-cell">
@@ -218,7 +222,9 @@ onMounted(() => {
           </van-checkbox>
         </div>
         <div class="cp-cell">
-          <van-button block round type="primary" native-type="submit">登 录</van-button>
+          <van-button block round type="primary" native-type="submit"
+            >登 录</van-button
+          >
         </div>
         <div class="cp-cell">
           <a href="javascript:;">忘记密码？</a>
